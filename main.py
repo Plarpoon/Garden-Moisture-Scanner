492e10b56bd2229ef9456662a943a28884d49d6f
Alex <plarpoon.dev@gmail.com>
2021-07-20 14:07:52 +0200

    set timer to an hour

--- main.py
+++ main.py
@@ -21,20 +21,19 @@ while True:
         print(moisture)
         drynessStatus = True
 
-        # Send plant moisture status to Pybytes
         pybytes.send_signal(2, "The Plant is Dry!")
 
     if moisture >= thresholdUp:
         print("The plant is wet!")
         print(moisture)
 
-        # Send plant moisture status to Pybytes
         pybytes.send_signal(2, "The Plant is Wet!")
 
-    if drynessStatus == True and celsius < 30:
+    #   Send result to PyBytes.
+    if drynessStatus == True and celsius <= 30:
         print("The plant is ready to be watered.")
         pybytes.send_signal(3, "Water it NOW!")
         pybytes.send_signal(1, celsius)
         print("sending: {}".format(celsius))
 
-    time.sleep(10)
+    time.sleep(3600)
