3ae9b5c9715d2943a1d6b2cd938a9e0a0cd64276
Alex <plarpoon.dev@gmail.com>
2021-07-23 02:38:02 +0200

    added comments and trimmed celsius number

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import machine
 import time
 
+#   These define which pin you are using on your Lopy4
+#   I suggest you open their documentation and check what you can and cannot use before doing any changes.
 adc = machine.ADC()
 MoisturePin = adc.channel(pin='P16')
 TempPin = adc.channel(pin='P20')
 
+#   These are the values to change if you want to adjust the moisture threshold yourself.
 thresholdUp = 400
 thresholdDown = 250
 
@@ -13,8 +16,10 @@ while True:
     millivoltsTemp = TempPin.voltage()
     drynessStatus = False
 
-    celsius = (millivoltsTemp - 500.0) / 10.0
-    print("The temperature is ", celsius, "Celsius")
+    celsius = (millivoltsTemp - 500.0) / 10.0                       #   Equation to calculate Celsius from millivolts
+    round(celsius, 3)                                               #   Do the rounding to only 3 digits
+
+    celsiustext = "The temperature is {} Celsius.".format(celsius)  #   Build the string in question
 
     if moisture <= thresholdDown:
         print("The plant is dry!")
@@ -28,8 +33,10 @@ while True:
     #   Send result to PyBytes.
     if drynessStatus == True and celsius <= 30:
         print("The plant is ready to be watered.")
-        pybytes.send_signal(3, "Water it NOW!")
-        pybytes.send_signal(1, celsius)
-        print("sending: {}".format(celsius))
+        pybytes.send_signal(3, "The plant is dry, water it NOW!")
+        pybytes.send_signal(1, celsiustext)
+        print("sending: {}".format(celsiustext))
 
-    time.sleep(3600)
+    #   This number decides how often you will receive a status report on Discord.
+    #   In this case you will receive notice every 15 minutes.
+    time.sleep(900)
